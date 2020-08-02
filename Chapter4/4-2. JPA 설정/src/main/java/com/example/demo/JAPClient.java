310a8a6006e1e50a44001d63e72eb05658bc04e9
SubAkBa <circle5926@gmail.com>
2020-08-02 18:28:12 +0900

    Chapter 4-2. JPA Configuration

---
+++ Chapter4/4-2. JPA 설정/src/main/java/com/example/demo/JAPClient.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.example.demo.domain.Board;
+
+public class JAPClient {
+	public static void main(String[] args) {
+		// EntityManager 생성
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Chapter04");
+		EntityManager em = emf.createEntityManager();
+
+		// Transaction 생성
+		EntityTransaction tx = em.getTransaction();
+
+		try {
+			// Transaction 시작
+			tx.begin();
+
+			Board board = new Board();
+			board.setTitle("JPA 제목");
+			board.setWriter("관리자");
+			board.setContent("JPA 글 등록 잘 되네요.");
+			board.setCreateDate(new Date());
+			board.setCnt(0L);
+
+			// 글 등록
+			em.persist(board);
+
+			// Transaction Commit
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			// Transaction Rollback
+			tx.rollback();
+		} finally {
+			em.close();
+			emf.close();
+		}
+	}
+}
