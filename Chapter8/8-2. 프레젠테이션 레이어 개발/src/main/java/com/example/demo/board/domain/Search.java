e843019ea29ab82edca65336f763aec239997f19
SubAkBa <circle5926@gmail.com>
2020-08-17 16:24:38 +0900

    Chpater 8-2. Develop Presentation Layer

---
+++ Chapter8/8-2. 프레젠테이션 레이어 개발/src/main/java/com/example/demo/board/domain/Search.java
@@ -0,0 +1,9 @@
+package com.example.demo.board.domain;
+
+import lombok.Data;
+
+@Data
+public class Search {
+	private String searchCondition;
+	private String searchKeyword;
+}
---
+++ Chapter8/8-2. 프레젠테이션 레이어 개발/src/main/java/com/example/demo/board/service/BoardService.java
@@ -0,0 +1,21 @@
+package com.example.demo.board.service;
+
+import org.springframework.data.domain.Page;
+
+import com.example.demo.board.domain.Board;
+import com.example.demo.board.domain.Search;
+
+public interface BoardService {
+
+	void insertBoard(Board board);
+
+	void updateBoard(Board board);
+
+	void deleteBoard(Board board);
+
+	Board getBoard(Board board);
+
+	Page<Board> getBoardList(Board board);
+
+	Page<Board> getBoardList(Search search);
+}
